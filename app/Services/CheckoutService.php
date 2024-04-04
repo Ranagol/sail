8e823cf2b7d35323edefc552768511662b338172
Andor Horvat <andorhorvat@gmail.com>
2024-04-04 12:01:47 +0200

    First two tests are working.

--- app/Services/CheckoutService.php
+++ app/Services/CheckoutService.php
@@ -41,11 +41,16 @@ class CheckoutService
                 'price' => $product->price
             ]);
 
-            dump(Cart::all()->toArray());
-
+            // dump(Cart::all()->toArray());
         }
     }
 
+    /**
+     * Calculate the total price of the cart. For all products in the cart.
+     * With the pricing rules applied.
+     * 
+     * @return float
+     */
     public function getTotal(): float
     {
         $cartProducts = Cart::query()
@@ -59,9 +64,74 @@ class CheckoutService
         $total = 0;
 
         foreach($cartProducts as $cartProduct){
-            $total += $cartProduct->price * $cartProduct->quantity;
+            
+            /**
+             * Find a rule for a given product. Example, find the rule for FR1.
+             * Example how one rule looks like:  'SR1' => ['bulkDiscount', 3, 4.50],
+             */
+            $rule = $this->pricingRules[$cartProduct->product_code] ?? null;
+
+            //If there is a rule for the given situation, calculate total with applying the rule
+            if(!is_null($rule)){
+
+                /**
+                 * This is tricky. Very.
+                 * 
+                 * $rule[0] is a variable that contains the name of the method to be called. The 
+                 * syntax $this->{$rule[0]} is used to call the method whose name is stored in 
+                 * $rule[0]. All in all, $this->{$rule[0]} is = $this->getOneFree() for example.
+                 * 
+                 * $rule[0] - the name of the rule
+                 * $rule[1] - the quantity of the products that have to be bought to get the discount
+                 * $rule[2] - the new price after the quantity discount
+                 * 
+                 */
+                $total += $this->{$rule[0]}(
+                    $cartProduct,
+                    $rule[1],//The quantity of the products that have to be bought to get the discount
+                    $rule[2]//The new price after the quantity discount
+                );
+            } else {
+                //If there is no rule, calculate the total without applying any rule
+                $total += $cartProduct->price * $cartProduct->quantity;
+            }
         }
 
         return $total; 
     }
+
+    /**
+     * If the customer buys one FR1, will get another FR1 for free.
+     *
+     * @param [type] $product
+     * @param [type] $quantity
+     * @param [type] $newPrice
+     * @return void
+     */
+    private function getOneFree($product, $quantity, $newPrice)
+    {
+        /**
+         * With the 'getOneFree' the qty is the key, this is what we must calculate. The qty changes.
+         * Because if the quantity is 2, customer pays for 1, with this logic below.
+         * If the quantity is 3, customer pays for 2, with this logic below... And so on.
+         * So, we must calculate the quantity that the customer pays for.
+         */
+        $quantity = floor($product->quantity / 2) + $product->quantity % 2;
+        return $total = $product->price * $quantity;
+    }
+
+    /**
+     * If customer buys 3 pieces of SR1, the price will be 4.50 instead of 5.00
+     *
+     * @param [type] $product
+     * @param [type] $quantity
+     * @param [type] $newPrice
+     * @return void
+     */
+    private function bulkDiscount($product, $quantity, $newPrice)
+    {
+        return $total = $product->price * $product->quantity;
+    }
+
+
 }
--- tests/Unit/CheckoutTest.php
+++ tests/Unit/CheckoutTest.php
@@ -11,6 +11,21 @@ class CheckoutTest extends TestCase
 {
     use RefreshDatabase;
 
+    /**
+     * These are the pricing rules that have to be applied.
+     * Every rule has 3 elements.
+     * 1. The name of the rule
+     * 2. The quantity of the products that have to be bought to get the discount
+     * 3. The new price after the quantity discount
+     */
+    public array $pricingRules = [
+        //If the customer buys one FR1, will get another FR1 for free
+        'FR1' => ['getOneFree', null, null],
+        //If customer buys 3 pieces of SR1, the price will be 4.50 instead of 5.00
+        'SR1' => ['bulkDiscount', 3, 4.50],
+        // 'CF1' => 'priceDrop'
+    ];
+
     public function setUp(): void
     {
         parent::setUp();
@@ -21,20 +36,13 @@ class CheckoutTest extends TestCase
 
     public function testCheckout1(): void
     {
-
-        $pricingRules = [
-            //If the customer buys one FR1, will get another FR1 for free
-            'FR1' => ['getOneFree'],
-            //If customer buys 3 pieces of SR1, the price will be 4.50 instead of 5.00
-            'SR1' => ['bulkDiscount', 3, 4.50],
-            'CF1' => 'priceDrop'
-        ];
-
         /**
-         * These are the items that have to be scanned judgging by the task list.
+         * These are the items that have to be scanned judgging by the task list:
          * FR1, SR1, FR1, FR1, CF1
+         * 
+         * Notice, that we passed here the $pricingRules array as an argument to the CheckoutService.
          */
-        $checkout = new CheckoutService($pricingRules);
+        $checkout = new CheckoutService($this->pricingRules);
         $checkout->scan('FR1');
         $checkout->scan('SR1');
         $checkout->scan('FR1');
@@ -42,6 +50,40 @@ class CheckoutTest extends TestCase
         $checkout->scan('CF1');
 
         $expectedTotal = 22.45;
+        $this->assertEquals($expectedTotal, $checkout->getTotal());
+    }
+
+    /**
+     * This is the second test case that has to be checked.
+     * FR1, FR1
+     *
+     * @return void
+     */
+    public function testCheckout2(): void
+    {
+        $checkout = new CheckoutService($this->pricingRules);
+        $checkout->scan('FR1');
+        $checkout->scan('FR1');
+
+        $expectedTotal = 3.11;
+        $this->assertEquals($expectedTotal, $checkout->getTotal());
+    }
+
+    /**
+     * This is the third test case that has to be checked.
+     * SR1, SR1, FR1, SR1
+     *
+     * @return void
+     */
+    public function testCheckout3(): void
+    {
+        $checkout = new CheckoutService($this->pricingRules);
+        $checkout->scan('SR1');
+        $checkout->scan('SR1');
+        $checkout->scan('FR1');
+        $checkout->scan('SR1');
+
+        $expectedTotal = 16.61;
         $this->assertEquals($expectedTotal, $checkout->getTotal());
     }
 }
